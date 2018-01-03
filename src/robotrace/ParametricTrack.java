bd112c0ff6235ac4cb86fa247e69570352f0d2e7
andrei <you@exm.com>
2018-01-03 19:22:58 +0100

    Added the terain and do some changes on the race track

--- src/robotrace/ParametricTrack.java
+++ src/robotrace/ParametricTrack.java
@@ -2,6 +2,7 @@ package robotrace;
 
 import com.jogamp.opengl.GL;
 import com.jogamp.opengl.GL2;
+import com.jogamp.opengl.GL2ES3;
 import com.jogamp.opengl.glu.GLU;
 import com.jogamp.opengl.util.gl2.GLUT;
 
@@ -18,7 +19,7 @@ public class ParametricTrack extends RaceTrack {
     @Override
     public void draw(GL2 gl, GLU glu, GLUT glut) {
         Vector tangent = new Vector(0,0,0);
-        double step = 0.02;
+        double step = 0.002;
         for(double j= -2; j<NR_LANES-1; j++){
                 gl.glBegin(gl.GL_LINE_LOOP);
                 for(double i=0; i<1; i += step){
@@ -31,22 +32,42 @@ public class ParametricTrack extends RaceTrack {
                 gl.glEnd();
         }
         
-        for(double j= -2; j<3; j++){
+           for(double j= -2; j<2; j++){
                 gl.glBegin(gl.GL_TRIANGLE_STRIP);
-                double pOffset = 0.0005;
-                for(double i=0; i<1; i = i+pOffset){
-                    
-                    Vector a = new Vector(0,0,0);
-                    a = getTangent(i).cross(new Vector(0,0,1));
-                    a.normalized();
-                    a.x = a.x*1.22;
-                    a.y = a.y*1.22;
+                
+                for(double i=0; i<=1 + 2*step; i = i+ 2*step){
+                   
+                    tangent = getTangent(i).cross(new Vector(0,0,1));
+                    tangent.x = tangent.x*LANE_WIDTH;
+                    tangent.y = tangent.y*LANE_WIDTH;
                     gl.glColor3f(0.5f, 0.0f, 1.0f);
-                    gl.glVertex3d(getPoint(i).x+2*j*a.x,getPoint(i).y+2*j*a.y,getPoint(i).z);
-                    gl.glVertex3d(getPoint(i+pOffset).x+(j+1)*a.x,getPoint(i+pOffset).y+(j+1)*a.y,getPoint(i+pOffset).z);
+                    gl.glVertex3d(getPoint(i).x+j*tangent.x,getPoint(i).y+j*tangent.y,getPoint(i).z);
+                    gl.glVertex3d(getPoint(i).x+(j+1)*tangent.x,getPoint(i).y+(j+1)*tangent.y,getPoint(i).z);
                 }
                 gl.glEnd();
             }
+           
+          for (int j = -2; j <= 2; j += 4) {                                  //only use lane curves -2 and 2, these represent the outside lanes
+                gl.glBegin(GL2ES3.GL_QUADS);                                           //this time quads are used, since the 4 points lay in the same plane
+                double pOffset = 0.01;                                          //set the offset, this defines how many quads are used
+                for (double i = 0; i < 1; i = i + pOffset) {                    
+                    Vector a = getTangent(i).cross(new Vector(0, 0, 1));        //as with the drawing of the tracks above, use vector a to later on find the correct lane
+                    a.normalized();
+                    a.x = a.x * LANE_WIDTH;
+                    a.y = a.y * LANE_WIDTH;
+                      gl.glVertex3d(getPoint(i).x + j * a.x, getPoint(i).y + j * a.y, getPoint(i).z - 2);                 //set the first vertex, located at z-1
+                     gl.glVertex3d(getPoint(i).x + j * a.x, getPoint(i).y + j * a.y, getPoint(i).z);                     //set the second vertex, located at z
+
+                    Vector b = getTangent(i + pOffset).cross(new Vector(0, 0, 1));  //as with the drawing of the tracks use vector b to find the correct lane, this time increment i again
+                    b.normalized();
+                    b.x = b.x * LANE_WIDTH;
+                    b.y = b.y * LANE_WIDTH;
+                    gl.glVertex3d(getPoint(i + pOffset).x + j * b.x, getPoint(i + pOffset).y + j * b.y, getPoint(i + pOffset).z);       //set the vertex, at point i with offset and z
+                            gl.glVertex3d(getPoint(i + pOffset).x + j * b.x, getPoint(i + pOffset).y + j * b.y, getPoint(i + pOffset).z - 2);   //set final vertex at point i with offset and z-1
+                }
+                gl.glEnd();                                                     //finish the quad
+            }
+         
     }
 
     @Override
--- src/robotrace/Terrain.java
+++ src/robotrace/Terrain.java
@@ -1,5 +1,6 @@
 package robotrace;
 
+import com.jogamp.opengl.GL;
 import com.jogamp.opengl.util.gl2.GLUT;
 import com.jogamp.opengl.GL2;
 import com.jogamp.opengl.glu.GLU;
@@ -19,7 +20,34 @@ class Terrain {
      * Draws the terrain.
      */
     public void draw(GL2 gl, GLU glu, GLUT glut) {
-
+        double dx = .5;                                                        
+        double dy = .5;                                                         
+                                                               
+        gl.glColor3f(1f, 0f, 0.5f);                                             
+        for(double x = -20; x< 20; x+= dx){
+            for(double y = -20; y< 20; y+= dy){
+                //define the points 
+                Vector p1 = new Vector(x, y, getHeight(x, y));
+                Vector p2 = new Vector(x+dx, y, getHeight(x+dx, y));
+                Vector p3 = new Vector(x, y+dy, getHeight(x, y+dy));            
+                Vector p4 = new Vector(x+dx, y+dy, getHeight(x+dx, y+dy));              
+       
+                gl.glBegin(GL.GL_TRIANGLE_STRIP);                                   
+                    gl.glVertex3d(p1.x(), p1.y(), p1.z());                                                            
+                    gl.glVertex3d(p2.x(), p2.y(), p2.z());                                                      
+                    gl.glVertex3d(p3.x(), p3.y(), p3.z());                        
+                    gl.glVertex3d(p4.x(), p4.y(), p4.z());                                                            
+                                        
+               gl.glEnd();
+            }  
+        }
     }
     
+      /**
+     * The height function used so we can get a hilly terrain
+     */
+    public double getHeight(double x, double y) {           
+        return 0.6*Math.cos(0.3*x+0.2*y)+0.4*Math.cos(x-0.5*y);                 //using the height function given in the assignment description
+    }
+   
 }
